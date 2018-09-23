950b8bcc56986c3a187f2af97a685d275306d2ff
wayne0324 <sw198603@gmail.com>
2018-09-23 13:42:26 -0500

    threeSum

--- src/LeetCode/ThreeSum.java
+++ src/LeetCode/ThreeSum.java
@@ -6,18 +6,24 @@ import java.util.List;
 
 public class ThreeSum {
 
-    public List<Integer> threeSum(int[] num) {
+    public List<List<Integer>> threeSum(int[] num) {
         Arrays.sort(num);
-        List<Integer> res = new LinkedList<>();
-        for (int i = 0; i < num.length - 2; i++) {
-            int lo = i + 1, hi = num.length - 1, sum = 0 - num[i];
-            while (lo < hi) {
-                if (num[lo] + num[hi] == sum) {
-                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
                 }
             }
         }
-
+        return res;
     }
 
 }
