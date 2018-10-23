563475b9e79efd209d31ed8dd4ea18b041252464
wayne0324 <sw198603@gmail.com>
2018-10-22 23:07:26 -0500

    ah

--- src/LeetCode/Tree/BST/LowestCommonAncestorBST.java
+++ src/LeetCode/Tree/BST/LowestCommonAncestorBST.java
@@ -1,5 +1,7 @@
 package LeetCode.Tree.BST;
 
+import LeetCode.Tree.TreeNode;
+
 //235. Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
 //
 //        According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
@@ -25,7 +27,16 @@ package LeetCode.Tree.BST;
 //        Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself
 //        according to the LCA definition.
 public class LowestCommonAncestorBST {
-    
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
 
 
 }
